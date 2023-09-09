f84cfe5ddcee87668e510035ada7c7f156f98f2e
blckvia <a.vyatkinn@icloud.com>
2023-09-09 21:32:39 +0300

    ex3.12 added

---
+++ ex3.12/main.go
@@ -0,0 +1,28 @@
+// ex3.11
+package main
+
+import (
+	"fmt"
+)
+
+func isAnagram(s, s2 string) bool {
+	if len(s) !=  len(s2) {
+		return false
+	}
+
+	arr := make([]rune, len(s))
+	for i, r := range s {
+		arr[i] = r
+	 }
+
+	 for i, letter := range s2 {
+		if letter != arr[i] {
+			return false
+		}
+	 }
+	 return true
+}
+
+func main() {
+	fmt.Println(isAnagram("ana", "ann"))
+}
