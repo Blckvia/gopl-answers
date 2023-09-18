123c3d977890d2a607d24ebd75e9df2334c2c33e
blckvia <a.vyatkinn@icloud.com>
2023-09-18 11:31:51 +0300

    ex4.9 added

---
+++ ex4.9/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	frequency := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	input.Split(bufio.ScanWords)
+
+	for input.Scan() {
+		frequency[input.Text()]++
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "frequency: %v", err)
+		os.Exit(1)
+	}
+	for word, n := range frequency {
+		fmt.Printf("%-30s %d\n", word, n)
+	}
+}
