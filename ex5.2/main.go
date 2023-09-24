20f9772e01875254ebe3a733d7d9ba33c084f54a
blckvia <a.vyatkinn@icloud.com>
2023-09-24 13:00:09 +0300

    ex5.2 has been added

---
+++ ex5.2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"os"
+)
+
+func freqTag(r io.Reader) (map[string]int, error) {
+	freq := make(map[string]int, 0)
+
+	z := html.NewTokenizer(os.Stdin)
+	var err error
+
+	for {
+		if z.Next() == html.ErrorToken {
+			break
+		}
+		name, _ := z.TagName()
+		if len(name) > 0 {
+			freq[string(name)]++
+		}
+	}
+	if err != io.EOF {
+		return freq, err
+	}
+	return freq, nil
+}
+
+func main() {
+	freq, err := freqTag(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for tag, count := range freq {
+		fmt.Printf("%4d %s\n", count, tag)
+	}
+}
