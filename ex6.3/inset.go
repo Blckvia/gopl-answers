0b71f5e3b8ccc949522a32b2b7310745c9fcf628
blckvia <a.vyatkinn@icloud.com>
2023-10-23 20:01:42 +0300

    feat: ex6.3 added

---
+++ ex6.3/inset.go
@@ -0,0 +1,37 @@
+// ex6.3 create Intersect, Difference and Symmetric methods
+package inset
+
+type IntSet struct {
+	words []uint64
+	count int
+}
+
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
