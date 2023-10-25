916932ff303e774161ac5e654de0added95677c3
blckvia <a.vyatkinn@icloud.com>
2023-10-25 17:50:00 +0300

    feat: ex7.2 added

---
+++ ex7.2/counter.go
@@ -0,0 +1,20 @@
+// ex7.2 wraps a writer to count written words.
+package counter
+
+import "io"
+
+type ByteCounter struct {
+	w       io.Writer
+	written int64
+}
+
+func (c *ByteCounter) Write(p []byte) (n int, err error) {
+	n, err = c.w.Write(p)
+	c.written += int64(n)
+	return
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	c := &ByteCounter{w, 0}
+	return c, &c.written
+}
