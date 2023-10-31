d45c853cc4c98c6510719e7fc99d08cec13e25b3
blckvia <a.vyatkinn@icloud.com>
2023-10-31 15:56:27 +0300

    feat: ex8.3 added

--- ex8.2/main.go
+++ ex8.2/main.go
@@ -57,5 +57,17 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
 }
