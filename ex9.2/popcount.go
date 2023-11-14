10157d2fa19521989284317988e4d9266ba8ac5d
blckvia <a.vyatkinn@icloud.com>
2023-11-14 15:25:40 +0300

    refactor: refactor init and loadTable

--- ex9.2/popcount.go
+++ ex9.2/popcount.go
@@ -7,24 +7,19 @@ import "sync"
 var pc [256]byte
 var loadTableOnce sync.Once
 
-func init() {
+func initTable() {
 	for i := range pc {
 		pc[i] = pc[i/2] + byte(i&1)
 	}
 }
 
-func loadTable() [256]byte {
-	loadTableOnce.Do(func() {
-		for i := range pc {
-			pc[i] = pc[i/2] + byte(i&1)
-		}
-	})
-	return pc
+func loadTable(b byte) byte {
+	loadTableOnce.Do(initTable)
+	return pc[b]
 }
 
 // PopCount returns the population count (number of set bits) of x.
 func PopCount(x uint64) int {
-	pc := loadTable()
 	return int(pc[byte(x>>(0*8))] +
 		pc[byte(x>>(1*8))] +
 		pc[byte(x>>(2*8))] +
